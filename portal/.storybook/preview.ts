819fbbe233da0ec67e57183732aca385275a30ce
Gustavo Arndt <gustavo.arndt@radixeng.com>
2025-06-05 15:33:51 -0300

    feat:setup basic dark and light theme

--- portal/.storybook/preview.ts
+++ portal/.storybook/preview.ts
@@ -1,11 +1,20 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from "@storybook/react-vite";
+
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+
+import { lightTheme, darkTheme } from "../src/utils/theme";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -13,9 +22,22 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      Provider: ThemeProvider,
+      themes: {
+        // Provide your custom themes here
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
---
+++ portal/src/utils/theme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material";
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
